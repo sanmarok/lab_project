b62ceb574824d0cec9bf508f4bf55dc19f1fb656
Martinez <san.martinezok@gmail.com>
2022-11-01 13:36:33 -0300

    Frontend ready 	deleted:    Proyecto/controler.php 	modified:   Proyecto/public/authentication.html 	modified:   Proyecto/public/script.js 	modified:   Proyecto/public/style.css

--- Proyecto/public/script.js
+++ Proyecto/public/script.js
@@ -1,11 +1,12 @@
-//Start login
-    function changeformtoregister(){
-        document.getElementById("conteiner_form_login_sm").style.display="none";
-        document.getElementById("conteiner_form_register_sm").style.display="block";
-    }
+//Start authentication
+    //  These functions switch the forms between them
+        function changeformtoregister(){
+            document.getElementById("conteiner_form_login_sm").style.display="none";
+            document.getElementById("conteiner_form_register_sm").style.display="block";
+        }
 
-    function changeformtologin(){
-        document.getElementById("conteiner_form_login_sm").style.display="block";
-        document.getElementById("conteiner_form_register_sm").style.display="none";
-    }
-//End login
+        function changeformtologin(){
+            document.getElementById("conteiner_form_login_sm").style.display="block";
+            document.getElementById("conteiner_form_register_sm").style.display="none";
+        }
+//End authentication
