7dce5fb9b4256c9e6994b8135011e3207472eb0f
Martinez <san.martinezok@gmail.com>
2022-11-01 13:08:05 -0300

    new file:   Proyecto/controler.php 	new file:   Proyecto/public/authentication.html 	new file:   Proyecto/public/image/authentication_background.jpg 	deleted:    Proyecto/public/image/la_harinera_logo.png 	deleted:    Proyecto/public/login.html 	modified:   Proyecto/public/script.js 	modified:   Proyecto/public/style.css

--- Proyecto/public/script.js
+++ Proyecto/public/script.js
@@ -1,3 +1,11 @@
 //Start login
+    function changeformtoregister(){
+        document.getElementById("conteiner_form_login_sm").style.display="none";
+        document.getElementById("conteiner_form_register_sm").style.display="block";
+    }
 
+    function changeformtologin(){
+        document.getElementById("conteiner_form_login_sm").style.display="block";
+        document.getElementById("conteiner_form_register_sm").style.display="none";
+    }
 //End login
