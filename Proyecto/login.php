4664e4d205b00f0ec361317d7e76729ded6fe6f9
Martinez <san.martinezok@gmail.com>
2022-11-02 10:18:11 -0300

    Frontend and backend ready to test
    	new file:   Proyecto/login.php
    	renamed:    Proyecto/public/authentication.html -> Proyecto/public/authentication.php
    	new file:   Proyecto/public/profile.php
    	new file:   Proyecto/public/profiletest.php
    	new file:   Proyecto/register.php
    	modified:   Proyecto/users.json

---
+++ Proyecto/login.php
@@ -0,0 +1,35 @@
+<?php 
+    class LoginUser{
+        private $username;
+        private $password;
+
+        public $error;
+        public $success;
+
+        private $storage = "../users.json";
+        private $stored_users; 
+
+        public function __construct($username, $password){
+            $this->username = $username;
+            $this->password = $password;
+
+            $this->stored_users = json_decode(file_get_contents($this->storage),true);
+            
+            $this->login();
+        }
+        
+        private function login(){
+            foreach($this->stored_users as $user){
+                if ($user['username'] == $this->username){
+                    if(password_verify($this->password, $user['password'])){
+                        session_start();
+                        $_SESSION['user'] = $this->username;
+                        header("location:profiletest.php"); exit();
+                    }
+                }
+            }
+
+            return $this->error = "Usuario o contraseña incorrecta";
+        }
+    }
+?>
