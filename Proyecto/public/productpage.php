9ea4fe71f4f1eeeee24f8d0c261c264f5d84c1aa
Martinez <san.martinezok@gmail.com>
2022-11-19 15:51:53 -0300

    Product pages ready

--- Proyecto/public/productpage.php
+++ Proyecto/public/productpage.php
@@ -22,11 +22,36 @@
         $storageproducts = json_decode($json_data,true);
 
         foreach ($storageproducts as $product) {
-            if ($product['productname'] == $_GET['product']){
-                echo($product['productname'].'<br>'.$product['price'].'<br>'.$product['description']);
+            if ($product['nombre'] == $_GET['product']){
+                echo('
+                    <div class="product_container_sm">
+                        <div>
+                            <img src="../public/image/'.$product['imagen'].'" alt="" srcset="">
+                        </div>
+                        <div>
+                            <p class="product_name">'.$product['nombre'].'</p>
+                            <p class="product_description">'.$product['descrip'].'</p>'
+                    );
+                
+          
+                    foreach($product['properties'] as $property){
+                        echo('<p class="data_tabulated">'.$property.'</p>');
+                    }
+
+                echo('
+                    <p class="price_tabulated">Precio: '.$product['preciokilo'].'/'.$product['unit'].'</p>
+                        </div>
+                    </div>
+                ');
                 break;
             }
         }
 ?>
+
+<?php
+
+?>
+
+
 </body>
 </html>
