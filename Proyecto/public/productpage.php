b91367702e66216348c4bd319bbfaff11b6fb9ce
Martinez <san.martinezok@gmail.com>
2022-11-19 15:43:36 -0300

    Products ready     *Productpage ready         -Frontend and backend ready         -Add properties
     Changes not staged for commit:
    	modified:   Proyecto/products.json
    	modified:   Proyecto/public/productpage.php
    	modified:   Proyecto/public/products.php
    	modified:   Proyecto/public/style.css

--- Proyecto/public/productpage.php
+++ Proyecto/public/productpage.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo ($_GET['product']);?></title>
-    <link rel="shortcut icon" href="./image/la_harinera_favicon.ico" type="image/x-icon">
+    <!--Icons-->
+      <link rel="shortcut icon" href="./image/la_harinera_favicon.ico" type="image/x-icon">
+      <script src="https://kit.fontawesome.com/dcd8a6e406.js" crossorigin="anonymous"></script>
     <!--Styles-->
         <link rel="stylesheet" href="normalize.css">
         <link rel="stylesheet" href="style.css">
@@ -13,36 +15,6 @@
         <script src="script.js"></script>
 </head>
 <body>
-
-<!-- <div class="products_container_sm">
-    <p>Productos</p>
-</div>
-
-<div class="products_table_sm">
-    <?php
-    /*
-        $json_data = file_get_contents('../products.json');
-
-        $storageproducts = json_decode($json_data,true);
-
-        foreach ($storageproducts as $product) {
-            echo('
-            <div class="product_card_sm">
-                <img src="../public/image/'.$product['image'].'" alt="" srcset="">
-                <div class="product_text_sm">
-                    <p class="product_name">'.$product['productname'].'</p>
-                    <br>
-                    <p class="product_description">'.$product['description'].'</p>
-                    <br>
-                    <p class="product_price">Precio: $'.$product['price'].'/'.$product['unit'].'</p>
-                    
-                </div>
-            </div>'
-        );
-        }
-    */
-    ?>
-</div>-->
 
 <?php
     $json_data = file_get_contents('../products.json');
