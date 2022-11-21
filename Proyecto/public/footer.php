fde766d2745d6ede596aab18a138b5ac89c2830f
adolf <adolfo@infinet.com.ar>
2022-11-21 17:21:18 -0300

    footer arreglos

--- Proyecto/public/footer.php
+++ Proyecto/public/footer.php
@@ -1,32 +1,49 @@
-<!-- Remove the container if you want to extend the Footer to full width. -->
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Icons-->
+      <link rel="shortcut icon" href="./image/la_harinera_favicon.ico" type="image/x-icon">
+      <script src="https://kit.fontawesome.com/dcd8a6e406.js" crossorigin="anonymous"></script>
+    <!--Styles-->
+        <link rel="stylesheet" href="normalize.css">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="            sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">         <!-- Si andas laburando fuera de csc esto te agrega los estilos de boostrap -->
+    <!--Scripts-->
+        <script src="script.js"></script>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="            sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">         <!-- Si andas laburando fuera de csc esto te agrega los estilos de boostrap -->
-<script src="https://kit.fontawesome.com/dcd8a6e406.js" crossorigin="anonymous"></script>
-<div class="container my-5">
+</head>
 
-  <footer class="text-center text-lg-start" style="background-color: #db6930;">
-    <div class="container d-flex justify-content-center py-5"> <h2>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
-        <i class="fab fa-facebook-f"></i>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
-        <i class="fab fa-youtube"></i>
-      </button> 
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
-        <a class="fab fa-instagram" href="https://www.instagram.com/reminn78/?hl=es-la"></a>
-      </button>
-      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b; >a la tumba de peron">
-        <i class="fab fa-twitter"></i></h2>
-      </button>
-    </div>
+<body>
+  <div class="container my-5">
 
-    <!-- Copyright -->
-    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2022 Copyright:
-     <h2> <a class="text-white" href="https://www.instagram.com/reminn78/?hl=es-la">Instagram de Proveedor</a></h2>
-    </div>
-    <!-- Copyright -->
-  </footer>
-  
-</div>
+    <footer class="text-center text-lg-start" style="background-color: #db6930;">
+      <div class="container d-flex justify-content-center py-5"> <h2>
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+          <a href="facebook.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+        </button>
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+          <a href="youtube.com" target="_blank" rel="noopener noreferrer"><i class="fab fa-youtube"></i></a>
+        </button> 
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+          <a class="fab fa-instagram" href="https://www.instagram.com/reminn78/?hl=es-la"></a>
+        </button>
+        <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;"></button>
+          <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></h2></a>
+        </button>
+      </div>
+      <!-- Copyright -->
+      <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2022 Copyright:
+      <h2> <a class="text-white" href="https://www.instagram.com/reminn78/?hl=es-la">Instagram del Proveedor</a></h2>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    
+  </div>
+
+
+</body>
 <!-- End of .container -->
