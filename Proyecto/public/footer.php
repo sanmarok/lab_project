8f441753a40f19186539376ceed92514d33c05d8
adolf <adolfo@infinet.com.ar>
2022-11-21 16:43:07 -0300

    footer ready for action

---
+++ Proyecto/public/footer.php
@@ -0,0 +1,32 @@
+<!-- Remove the container if you want to extend the Footer to full width. -->
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="            sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">         <!-- Si andas laburando fuera de csc esto te agrega los estilos de boostrap -->
+<script src="https://kit.fontawesome.com/dcd8a6e406.js" crossorigin="anonymous"></script>
+<div class="container my-5">
+
+  <footer class="text-center text-lg-start" style="background-color: #db6930;">
+    <div class="container d-flex justify-content-center py-5"> <h2>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+        <i class="fab fa-facebook-f"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+        <i class="fab fa-youtube"></i>
+      </button> 
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b;">
+        <a class="fab fa-instagram" href="https://www.instagram.com/reminn78/?hl=es-la"></a>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" style="background-color: #54456b; >a la tumba de peron">
+        <i class="fab fa-twitter"></i></h2>
+      </button>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2022 Copyright:
+     <h2> <a class="text-white" href="https://www.instagram.com/reminn78/?hl=es-la">Instagram de Proveedor</a></h2>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  
+</div>
+<!-- End of .container -->
